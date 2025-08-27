17ae9e2798b24104f4bc2a649b3968719895f03c
SaimumAoyon1134 <saimum681@gmail.com>
2025-08-27 13:19:46 +0600

    ballo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hearts = document.getElementsByClassName("heartIcon");
 for (let heart of hearts) {
-    heart.addEventListener("click", function() {
+    heart.addEventListener("click", function(e) {
+        e.preventDefault();
        document.getElementById("loveCount").innerText = parseInt(document.getElementById("loveCount").innerText) + 1;
     
 })};
@@ -8,10 +9,20 @@ for (let heart of hearts) {
 
 const copyButtons = document.getElementsByClassName("copyButton");
 for (let button of copyButtons) {
-    button.addEventListener("click", function() {
-
+    button.addEventListener("click", function(e) {
+        e.preventDefault();
          text=button.parentElement.parentElement.children[3].innerText;
          navigator.clipboard.writeText(text);
          document.getElementById("copyCount").innerText = parseInt(document.getElementById("copyCount").innerText) + 1;
          alert(text + " copied to clipboard");
     })}; 
+const callButtons = document.getElementsByClassName("callButton");
+for (let button of callButtons) {
+    button.addEventListener("click",function(e){
+        e.preventDefault();
+        alert("Calling " + button.parentElement.parentElement.children[1].innerText);
+        document.getElementById("historyContent").appendChild(document.createElement("box")).innerText = button.parentElement.parentElement.children[1].innerText + new Date().toLocaleString();
+        
+
+    })
+};
