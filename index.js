b4d3e45fa4419f9af79d971b36137986ebb8a75d
SaimumAoyon1134 <saimum681@gmail.com>
2025-08-27 13:59:52 +0600

    kochu

--- index.js
+++ index.js
@@ -21,8 +21,34 @@ for (let button of callButtons) {
     button.addEventListener("click",function(e){
         e.preventDefault();
         alert("Calling " + button.parentElement.parentElement.children[1].innerText);
-        document.getElementById("historyContent").appendChild(document.createElement("box")).innerText = button.parentElement.parentElement.children[1].innerText + new Date().toLocaleString();
+        const historyBox =document.getElementById("historyContent");
+        const box=document.createElement("div");
+        box.className=`flex flex-row text-[0.8rem] justify-between items-center  p-[10px] m-[10px] rounded-xl bg-gray-100`;
+        const left=document.createElement("div");
+        left.className=`flex flex-col`;
+        const leftUpper=document.createElement("p");
+        leftUpper.innerText=button.parentElement.parentElement.children[1].innerText;
+        leftUpper.className=`font-bold`;
+        const leftDown=document.createElement("p");
+        leftDown.innerText=button.parentElement.parentElement.children[3].innerText;
+        left.appendChild(leftUpper);
+        left.appendChild(leftDown);
+        box.appendChild(left);
         
 
+        const right=document.createElement("div");
+        right.className=`flex justify-center items-center`;
+        const time=document.createElement("p");
+        const currentDate = new Date();
+        let hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+        const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12; // Convert to 12-hour format
+        hours = hours.toString().padStart(2, '0');
+        time.innerText = `${hours}:${minutes}:${seconds} ${ampm}`;
+        right.appendChild(time);
+        box.appendChild(right);
+        historyBox.appendChild(box);
     })
 };
